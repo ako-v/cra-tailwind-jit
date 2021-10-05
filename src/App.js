f774642ea34e8511d4826755cd70099210dcdfe0
ako <ako.vnj@gmail.com>
2021-10-05 11:51:24 +0330

    moved tailwind.css and index.css to styles folder.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 function App() {
   return (
-    <div className="bg-blue-400 h-screen grid">
+    <div className="bg-red-400 h-screen grid">
       <div className="w-3/4 my-auto ml-20">
         <h1 className="text-5xl font-bold mb-10 text-white">JIT mode is cool</h1>
         <p className="text-white">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo officia earum ducimus neque obcaecati consequuntur ratione accusamus at officiis tempore,
-          magnam non debitis fugit unde alias id quidem necessitatibus.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo officia earum ducimus neque obcaecati consequuntur ratione accusamus at officiis
+          tempore, magnam non debitis fugit unde alias id quidem necessitatibus.
         </p>
       </div>
     </div>
